40d97a415371072cf8fbe824089ee96fc588559f
Aakash bhardwaj <54072018+Aakashbhardwaj27@users.noreply.github.com>
2020-05-04 16:32:10 +0530

    Create RightColumn.js

---
+++ src/Components/RightColumn.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import RecentViewed from"./RecentViewed"
+
+function RightColumn(){
+
+    return(
+      
+        <div className="RightColumn">
+          
+        </div>
+    
+    )
+}
+
+export default RightColumn
