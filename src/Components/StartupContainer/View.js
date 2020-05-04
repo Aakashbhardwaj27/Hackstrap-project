84d0f32a1f4cbcff005acf3ec29ffada8acf1025
Aakash bhardwaj <54072018+Aakashbhardwaj27@users.noreply.github.com>
2020-05-04 16:32:23 +0530

    Create View.js

---
+++ src/Components/StartupContainer/View.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+function View(){
+    return(
+        <div className="View"><h6>234 views</h6>
+        
+        {/* fetch the api data  */}
+        </div>
+    )
+}
+
+export default View
