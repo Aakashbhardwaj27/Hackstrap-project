2ff144e501fa662cb9b0c47f5c7da0c6fbb11162
Aakash bhardwaj <54072018+Aakashbhardwaj27@users.noreply.github.com>
2020-05-04 16:32:13 +0530

    Create searchBox.js

---
+++ src/Components/searchBox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../Components/App.css'
+
+function SearchBox(){
+return(
+
+    <div >
+        <form >
+        <input type="text" placeholder ="Your Deal Space" className="searchBar"></input>
+       
+       
+        </form>
+
+    </div>
+)
+
+}
+
+export default SearchBox
