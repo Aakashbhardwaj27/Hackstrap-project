5cc2408288030e3fa482bbce0c3c6bca65fdf51a
Aakash bhardwaj <54072018+Aakashbhardwaj27@users.noreply.github.com>
2020-05-04 16:32:02 +0530

    Create RecentViewed.js

---
+++ src/Components/RecentViewed.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './App.css'
+
+function RecentViewed(){
+
+return(
+   <div className="RecentViewed">
+     <div className="Title"><h5>Recently Viewed</h5></div>
+
+
+   </div>
+
+
+)
+}
+
+export default RecentViewed
